1e2579c443dd6368c3fcf16c650415a993d1ec95
zteezy19 <zteezy.dev@gmail.com>
2024-12-07 22:59:16 +0800

    Added day2 pt2 & cleanedup

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,3 +1,4 @@
+// part 1
 pub fn is_safe_report(numbers: &Vec<i32>) -> bool {
     if numbers.len() < 2 {
         return true;
@@ -14,4 +15,21 @@ pub fn is_safe_report(numbers: &Vec<i32>) -> bool {
         };
         correct_direction && diff >= 1 && diff <= 3
     })
+}
+
+// part 2
+pub fn is_safe_with_dampener(numbers: &Vec<i32>) -> bool {
+    if is_safe_report(numbers) {
+        return true;
+    }
+
+    for i in 0..numbers.len() {
+        let mut modified = numbers.clone();
+        modified.remove(i);
+        if is_safe_report(&modified) {
+            return true;
+        }
+    }
+
+    false
 }
--- day2/target/debug/deps/day2-5cb990b70d2b671b.d
+++
@@ -1,5 +0,0 @@
-/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/deps/libday2-5cb990b70d2b671b.rmeta: src/main.rs
-
-/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/deps/day2-5cb990b70d2b671b.d: src/main.rs
-
-src/main.rs:
