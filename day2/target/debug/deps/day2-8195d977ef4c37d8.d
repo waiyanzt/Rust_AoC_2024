57edd2dd1167cb08314f4be7e807408bf797a18c
zteezy19 <zteezy.dev@gmail.com>
2024-12-07 22:44:39 +0800

    Added day2 pt1

---
+++ day2/target/debug/deps/day2-8195d977ef4c37d8.d
@@ -0,0 +1,5 @@
+/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/deps/day2-8195d977ef4c37d8: src/main.rs
+
+/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/deps/day2-8195d977ef4c37d8.d: src/main.rs
+
+src/main.rs:
---
+++ day2/target/debug/libday2.d
@@ -0,0 +1,1 @@
+/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/libday2.rlib: /home/ztz11/Documents/Github/AoC_2024/day2/src/lib.rs
