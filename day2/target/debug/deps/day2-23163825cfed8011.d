050d0a04db86f9d154cd1144f610804523e8f27f
zteezy19 <zteezy.dev@gmail.com>
2024-12-09 00:58:17 +0800

    Added day3 pt2 & cleanedup

---
+++ day2/target/debug/deps/day2-23163825cfed8011.d
@@ -0,0 +1,5 @@
+/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/deps/libday2-23163825cfed8011.rmeta: src/lib.rs
+
+/home/ztz11/Documents/Github/AoC_2024/day2/target/debug/deps/day2-23163825cfed8011.d: src/lib.rs
+
+src/lib.rs:
