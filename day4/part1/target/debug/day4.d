ee9b11e884d834bbe7c67f432271991056ec944c
zteezy19 <zteezy.dev@gmail.com>
2024-12-09 03:38:13 +0800

    Added day4 pt1 and init p2

---
+++ day4/part1/target/debug/day4.d
@@ -0,0 +1,1 @@
+/home/ztz11/Documents/Github/AoC_2024/day4/part1/target/debug/day4: /home/ztz11/Documents/Github/AoC_2024/day4/part1/src/grid.rs /home/ztz11/Documents/Github/AoC_2024/day4/part1/src/main.rs
---
+++ day4/part1/target/debug/deps/day4-6947e26cd3d43309.d
@@ -0,0 +1,6 @@
+/home/ztz11/Documents/Github/AoC_2024/day4/part1/target/debug/deps/libday4-6947e26cd3d43309.rmeta: src/main.rs src/grid.rs
+
+/home/ztz11/Documents/Github/AoC_2024/day4/part1/target/debug/deps/day4-6947e26cd3d43309.d: src/main.rs src/grid.rs
+
+src/main.rs:
+src/grid.rs:
