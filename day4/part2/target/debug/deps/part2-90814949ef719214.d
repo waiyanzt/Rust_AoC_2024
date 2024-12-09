584b029c564a4446ca97254d07d407d99759d0e5
zteezy19 <zteezy.dev@gmail.com>
2024-12-09 18:04:01 +0800

    Cleanup

--- day4/part2/target/debug/deps/part2-90814949ef719214.d
+++
@@ -1,5 +0,0 @@
-/home/ztz11/Documents/Github/AoC_2024/day4/part2/target/debug/deps/libpart2-90814949ef719214.rmeta: src/main.rs
-
-/home/ztz11/Documents/Github/AoC_2024/day4/part2/target/debug/deps/part2-90814949ef719214.d: src/main.rs
-
-src/main.rs:
--- day4/part2/target/debug/deps/part2-ad04d4546d3e83c2.d
+++
@@ -1,5 +0,0 @@
-/home/ztz11/Documents/Github/AoC_2024/day4/part2/target/debug/deps/libpart2-ad04d4546d3e83c2.rmeta: src/main.rs
-
-/home/ztz11/Documents/Github/AoC_2024/day4/part2/target/debug/deps/part2-ad04d4546d3e83c2.d: src/main.rs
-
-src/main.rs:
